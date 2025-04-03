a520b201e93b22481b5e9275db75f1cfc49c01f9
stepdugas <131499172+stepdugas@users.noreply.github.com>
2025-04-03 16:52:36 -0400

    Initial Commit

---
+++ MeditationApp/MeditationAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MeditationAppApp.swift
+//  MeditationApp
+//
+//  Created by Stephanie Dugas on 4/3/25.
+//
+
+import SwiftUI
+
+@main
+struct MeditationAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MeditationAppTests/MeditationAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  MeditationAppTests.swift
+//  MeditationAppTests
+//
+//  Created by Stephanie Dugas on 4/3/25.
+//
+
+import Testing
+@testable import MeditationApp
+
+struct MeditationAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
